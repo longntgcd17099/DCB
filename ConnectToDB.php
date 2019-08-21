4750a8517626384ad380007e560dadc5891c2c9e
LONGLONG <LONGLONG@DESKTOP-DBVKU6P>
2019-08-21 15:27:50 +0700

    thaydoi.index

--- ConnectToDB.php
+++ ConnectToDB.php
@@ -30,7 +30,18 @@
               <?php
                 echo '<p>Database ATN</p>';
                 echo getenv("dbname");
-                $pdo = new PDO("pgsql:dbname=d5c9hsgmdvbs20;host=ec2-54-227-245-146.compute-1.amazonaws.com","dphhigcaqlnlbo", "2d9e255af360659b3de3c0428221be071372fbfdc09c6a64422cc8a73395b004" );
+                $pdo = new PDO("pgsql:" . sprintf(
+         "host=ec2-54-227-245-146.compute-1.amazonaws.com;
+      port=5432;
+      user=dphhigcaqlnlbo;
+      password=2d9e255af360659b3de3c0428221be071372fbfdc09c6a64422cc8a73395b004;
+      dbname=d5c9hsgmdvbs20",
+            $db["host"],
+            $db["port"],
+            $db["user"],
+            $db["pass"],
+            ltrim($db["path"], "/")     
+        ));
                 if ($pdo === false) {
                   echo "ERROR: Could not connect Database";
                 }
