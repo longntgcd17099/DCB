651f246c0ed85b9518147f91cb4674bcf4334218
LONGLONG <LONGLONG@DESKTOP-DBVKU6P>
2019-08-22 11:53:57 +0700

    thaydoi.index

--- ConnectToDB.php
+++ ConnectToDB.php
@@ -4,6 +4,18 @@
   
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="style.css">
+  <style type="text/css">
+    body {
+        width: 100%;
+        height: 100%;
+        background: url(gunn.jpg) no-repeat;
+        background-size: cover;
+    }
+
+    p {
+        text-align: center;
+    }
+</style>
 </head>
 <body>
     <div class="row justify-content-center">
