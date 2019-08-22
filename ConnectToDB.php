a187f8e8f6f29f5a48d55a76b98cfccfccd5f608
LONGLONG <LONGLONG@DESKTOP-DBVKU6P>
2019-08-22 11:31:39 +0700

    thaydoi.index

--- ConnectToDB.php
+++ ConnectToDB.php
@@ -4,23 +4,11 @@
   
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="style.css">
-  <style type="text/css">
-  body {
-    width: 100%;
-    height: 100%;
-    background: url(gun.png) no-repeat;
-    background-size: cover;
-  }
-
-  p {
-    text-align: center;
-  }
-</style>
 </head>
 <body>
     <div class="row justify-content-center">
             <div class="col-9">
-              <h1>Database For Store</h1>
+              <h1>ATN Shop</h1>
 
               <?php
                ini_set('display_errors', 1);
@@ -28,29 +16,19 @@
               ?>
 
               <?php
-              $db = parse_url(getenv("DATABASE_URL"));
-                $pdo = new PDO("pgsql:". sprintf(
-         "host=ec2-54-227-245-146.compute-1.amazonaws.com;
-      port=5432;
-      user=dphhigcaqlnlbo;
-      password=2d9e255af360659b3de3c0428221be071372fbfdc09c6a64422cc8a73395b004;
-      dbname=d5c9hsgmdvbs20",
-            $db["host"],
-            $db["port"],
-            $db["user"],
-            $db["pass"],
-            ltrim($db["path"], "/")     
-        ));
+                echo '<p>The DB exists</p>';
+                echo getenv("dbname");
+                $pdo = naew PDO("pgsql:dbname=d5c9hsgmdvbs20;host=ec2-54-227-245-146.compute-1.amazonaws.com","dphhigcaqlnlbo", "2d9e255af360659b3de3c0428221be071372fbfdc09c6a64422cc8a73395b004" );
                 if ($pdo === false) {
                   echo "ERROR: Could not connect Database";
                 }
-                $sql = 'SELECT * FROM public.supplier';
+                $sql = 'SELECT * FROM public."supplier"';
                 $stmt = $pdo->prepare($sql);
                 //Thiết lập kiểu dữ liệu trả về
                 $stmt->setFetchMode(PDO::FETCH_ASSOC);
                 $stmt->execute();
                 $resultSet = $stmt->fetchAll();
-                echo '<p>Product Information:</p>';
+                echo '<p>Supplier Information:</p>';
               ?>
               <div class="table-responsive">
                 <table class="table">
@@ -67,7 +45,7 @@
                       <tr class="odd gradeX">
                         <td><?php echo $value['Supplier_ID']; ?></td>
                         <td><?php echo $value['Supplier_Name']; ?></td>
-                        <td><?php echo $value['Suppier_Address']; ?></td>
+                        <td><?php echo $value['Supplier_Address']; ?></td>
                         <td><?php echo $value['Supplier_Phone']; ?></td>
                       </tr>
                     <?php endforeach; ?>
